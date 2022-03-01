cec4a7900fce9d644a09e51c2f12944ca63b50e8
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2022-03-02 03:32:35 +0600

    phone details added

--- JS/app.js
+++ JS/app.js
@@ -12,6 +12,7 @@ const searchPhone = () => {
     // display spinner 
     toggleSpinner('block');
     document.getElementById('search-result').innerHTML = '';
+    document.getElementById('phone-detail').innerHTML = '';
     // clear input field 
     searchField.value = '';
 
@@ -43,19 +44,18 @@ const displayPhones = data => {
             let div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-        <div onclick="loadPhoneDetails('${phones.slug}')" class="card h-100 rounded-3 card-background">
+        <div class="card h-100 rounded-3 card-background">
               <img src="${phones.image}" class="card-img-top p-4 card-img-bg" alt="...">
                 <div class="card-body">
-                    <h4 class="card-title text-danger">Phone Brand:<span class="text-dark"> ${phones.phone_name}</span></h4>
-                    <p class="card-text text-success">Model Name: <span class="text-dark"> ${phones.brand}</span></p>
+                    <h4 class="card-title text-danger">Phone Name:<span class="text-dark"> ${phones.phone_name}</span></h4>
+                    <p class="card-text text-success">Brand Name: <span class="text-dark"> ${phones.brand}</span></p>
                 </div>
                     <div class="d-flex justify-content-center p-3">
-                        <button onclick="loadPhoneDetails(${phones.slug})" type="button" class="btn btn-primary btn-sm">Show More Details</button>
+                        <button onclick="loadPhoneDetails('${phones.slug}')" type="button" class="btn btn-primary btn-sm">Show More Details</button>
                     </div>
         </div>
         `;
             searchResult.appendChild(div);
-
 
         });
         toggleSpinner('none');
@@ -63,5 +63,39 @@ const displayPhones = data => {
 }
 
 const loadPhoneDetails = phoneId => {
-    console.log(phoneId)
+    document.getElementById('phone-detail').innerHTML = '';
+
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data))
+}
+const displayPhoneDetail = phone =>{
+    // console.log(phone);
+    const phoneDetails = document.getElementById('phone-detail');
+    let releaseDate = phone.releaseDate;
+
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card rounded-3 d-flex flex-row p-3">
+            <img src="${phone.image}" class="p-2 card-image" style="height: 75%" alt="...">
+             <div class="card-body">
+                  <h2 class="card-title text-Success">Phone Name: <span class="text-dark">${phone.brand} ${phone.name}</span></h2>
+                     <h4 class="card-text text-success">Release Date: <span class="text-dark">${phone.releaseDate}</span></h4>
+                     <h6 class="card-text text-primary">ChipSet: <span class="text-dark">${phone.mainFeatures.chipSet}</span></h6>
+                     <h6 class="card-text text-primary">Display Size: <span class="text-dark">${phone.mainFeatures.displaySize}</span></h6>
+                     <h6 class="card-text text-primary">Memory Size: <span class="text-dark">${phone.mainFeatures.memory}</span></h6>
+                     <h6 class="card-text text-primary">Storage Size: <span class="text-dark">${phone.mainFeatures.storage}</span></h6>
+                     <h6 class="card-text text-primary">Bluetooth: <span class="text-dark">${phone.others.Bluetooth}</span></h6>
+                     <h6 class="card-text text-primary">GPS: <span class="text-dark">${phone.others.GPS}</span></h6>
+                     <h6 class="card-text text-primary">NFC: <span class="text-dark">${phone.others.NFC}</span></h6>
+                     <h6 class="card-text text-primary">Radio: <span class="text-dark">${phone.others.Radio}</span></h6>
+                     <h6 class="card-text text-primary">USB: <span class="text-dark">${phone.others.USB}</span></h6>
+                     <h6 class="card-text text-primary">WLAN: <span class="text-dark">${phone.others.WLAN}</span></h6>
+             </div>
+    </div>
+    `;
+
+    phoneDetails.appendChild(div);
 }
